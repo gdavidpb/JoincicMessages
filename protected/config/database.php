38b1c0d8f4d59be63fcb0c770738cec10125affc
obando777@gmail.com <obando777@gmail.com>
2014-05-24 17:34:46 -0430

    * begin with the app

---
+++ protected/config/database.php
@@ -0,0 +1,10 @@
+<?php return array(
+
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=miguelob_joincicc',
+			'emulatePrepare' => true,
+			'username' => 'miguelob_joincic',
+			'password' => 'j0rn4d45',
+			'charset' => 'utf8',
+		),
+);
