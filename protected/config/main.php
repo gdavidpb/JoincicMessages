d9aa778394f0d4b8200b468552f00812ae50a8c1
obando777@gmail.com <obando777@gmail.com>
2014-05-24 20:36:29 -0430

    Active users login.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -69,6 +69,7 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
+		'users' => include(dirname(__FILE__).'/users.php'),
 		// this is used in contact page
 		'adminEmail'=>'contacto@joincic.com.ve',
 		'cuenta_token' => '0f0e8682057a87d03db1c5f0364ddcd0',
