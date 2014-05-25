0875baa6e8e26af672033e47cfc756b88045129f
obando777@gmail.com <obando777@gmail.com>
2014-05-24 20:54:53 -0430

    Message con el estilo chuleado del wordpress

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,24 +1,38 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es-ES"><head profile="http://gmpg.org/xfn/11">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+		<!-- add-ons -->
+	<link rel="pingback" href="http://joincic.com.ve/xmlrpc.php">
+	<link rel="stylesheet" href="http://joincic.com.ve/wp-content/themes/joincic-7ma/style.css" type="text/css" media="screen">
+
+	<link href="http://fonts.googleapis.com/css?family=Dosis" rel="stylesheet" type="text/css">
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
 
-<body>
+	</head>
+
+<body >
+
+
+<!-- calling header -->
+	<div id="headerWrapper">
+		<div id="header">
+
+			<center>
+				<div id="logo"></div>
+				<h2 title="JORNADAS INTERUNIVERSITARIAS DE CIENCIAS DE LA COMPUTACIÓN">JORNADAS INTERUNIVERSITARIAS DE CIENCIAS DE LA COMPUTACIÓN</h2>
+			</center>
+
+			<div class="clear"></div>
+		</div>
+	</div>
+<!-- ending header -->
 
 <div class="container" id="page">
 
@@ -31,9 +45,9 @@
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
-
-
 </div><!-- page -->
 
-</body>
-</html>
+		
+</body></html>
+
+
