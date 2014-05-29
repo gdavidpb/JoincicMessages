0e7229ddc5ccce80d71d303849e6f66433cbc6e5
MiguelObando <miguelobando@wattopoststudio.com>
2014-05-29 16:02:18 -0430

    * fix

--- protected/views/site/fixls.php
+++ protected/views/site/fixls.php
@@ -1,4 +1,4 @@
-<form action="upload_file.php" method="post"
+<form method="post"
 enctype="multipart/form-data">
 <label for="upload_file">Archivo de XLS :</label>
 <input type="file" name="upload_file" id="file"><br>
